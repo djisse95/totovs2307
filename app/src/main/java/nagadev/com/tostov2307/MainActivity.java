ca7f145568f959885006af8c7f2afc392fed66b0
Samnang <samnang.n@ligercambodia.org>
2017-11-22 14:27:35 +0700

    jjj

--- app/src/main/java/nagadev/com/tostov2307/MainActivity.java
+++ app/src/main/java/nagadev/com/tostov2307/MainActivity.java
@@ -1,15 +1,37 @@
 package nagadev.com.tostov2307;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button samnang, vuthy, visal, mengthong, jibou;
+    public Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        samnang = (Button) findViewById(R.id.samnang);
+        vuthy = (Button) findViewById(R.id.vuthy);
+        visal = (Button) findViewById(R.id.visal);
+        mengthong = (Button) findViewById(R.id.mengthong);
+        jibou = (Button) findViewById(R.id.jibu);
+
+        samnang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
